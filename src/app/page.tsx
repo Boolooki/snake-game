0ec246df88c5dab803209935e6c93d42558c2bd7
Boarbasboarboi <tripidokintasuwan@gmail.com>
2025-09-17 18:48:43 +0700

    UI fixed name char

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,11 +42,17 @@ export default function Home() {
         <div className="fixed inset-0 bg-green-300 bg-opacity-60 flex items-center justify-center z-50">
           <div className="bg-white p-6 rounded shadow text-center">
             <h2 className="text-lg font-bold mb-4">Enter your name to start</h2>
-            
+            {username.length >= 10 && (
+            <p className="text-sm text-red-600 mt-1">
+              Max 10 characters allowed
+            </p>
+          )}
+
             <input
               type="text"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
+              maxLength={10}
               className="px-3 py-2 border rounded w-64 text-center mb-4"
               placeholder="Maximum 10 character"
             />
@@ -59,8 +65,8 @@ export default function Home() {
               Start Game
             </button>
           </div>
-          
         </div>
+        
       )}
 
       <h1 className="text-3xl font-bold">Snake Game</h1>
