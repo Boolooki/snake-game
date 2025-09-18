cfaf21aaaefc471c41b33e4fb946be5c12f16b72
Boarbasboarboi <tripidokintasuwan@gmail.com>
2025-09-18 16:12:42 +0700

    Time in  leaderboard is now much more make sense

--- src/components/LeaderboardModal.tsx
+++ src/components/LeaderboardModal.tsx
@@ -1,19 +1,29 @@
-import { useLeaderboard } from '../hooks/useLeaderboard';
+import { useLeaderboard } from "../hooks/useLeaderboard";
 
 export default function LeaderboardModal({ onClose }: { onClose: () => void }) {
   const { data, loading, error } = useLeaderboard();
+
+  function formatDuration(seconds: number): string {
+    const mins = Math.floor(seconds / 60);
+    const secs = (seconds % 60).toFixed(1);
+
+    if (mins > 0) {
+      return `${mins} minute${mins > 1 ? "s" : ""} ${secs} seconds`;
+    }
+    return `${secs} seconds`;
+  }
 
   return (
     <div className="fixed inset-0 flex justify-center items-center z-50">
       <div className="bg-green-200 p-6 rounded shadow-lg w-80">
         <div className="flex justify-between items-center mb-4">
-        <h2 className="text-xl font-bold">Leaderboard</h2>
-        <button
-          onClick={onClose}
-          className=" bg-gray-300 px-3 py-1 rounded hover:bg-gray-400"
-        >
-          X
-        </button>
+          <h2 className="text-xl font-bold">Leaderboard</h2>
+          <button
+            onClick={onClose}
+            className=" bg-gray-300 px-3 py-1 rounded hover:bg-gray-400"
+          >
+            X
+          </button>
         </div>
         {loading && <p>Loading...</p>}
         {error && <p>Error occurred</p>}
@@ -21,14 +31,15 @@ export default function LeaderboardModal({ onClose }: { onClose: () => void }) {
         <ul className="space-y-2">
           {data?.map((entry, i) => (
             <li key={i} className="flex justify-between gap-4">
-              <span>{i + 1}. {entry.username}</span>
+              <span>
+                {i + 1}. {entry.username}
+              </span>
               <span>{entry.score}</span>
-              <span>{(entry.duration / 1000).toFixed(1)}s</span>
+              tsx
+              <span>{formatDuration(entry.duration / 1000)}</span>
             </li>
           ))}
         </ul>
-
-        
       </div>
     </div>
   );
