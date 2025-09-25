1fac305e58f10af82e7063b0be781044817ff86d
Boarbasboarboi <tripidokintasuwan@gmail.com>
2025-09-25 15:09:07 +0700

    Board is now a bit prettier

--- src/components/game/Snake.tsx
+++ src/components/game/Snake.tsx
@@ -8,7 +8,7 @@ export default function Snake({ segments, isEnergyShield, isSpeedBurst }: { segm
       {segments.map((segment, index) => (
         <div
           key={index}
-          className={'bg-green-500' + (isEnergyShield ? ' border-4 border-blue-300' : '') + (isSpeedBurst ? ' animate-pulse' : '') + ' rounded-full'}
+          className={'rounded-full bg-gradient-to-r from-green-600 to-green-300 shadow-md' + (isEnergyShield ? ' border-4 border-blue-300' : '') + (isSpeedBurst ? ' animate-pulse' : '')}
           style={{
             gridColumnStart: segment.x,
             gridRowStart: segment.y,
